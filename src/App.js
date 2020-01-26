6845be8910edeba44c5a15ad4113b15aae004774
Paras Rawat <parasmyname@gmail.com>
2020-01-26 23:45:37 +0530

    Understanding fetch api and promises in Javascript

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component{
@@ -7,34 +7,29 @@ class App extends Component{
     super();
     this.state={
      monsters: [
-       {
-         name: 'FRANKESTIEN',
-         id: 'asc1'
-       },
-       {
-        name: 'DRACULA',
-        id: 'asc2'
-      },
-      {
-        name: 'ZOMBIE',
-        id: 'asc3'
-      }
+
      ]
     }
   }
-  //Anything in the {} means that is a expression. Although it may look like a normal html but it is not that, it is JSX ...jsx attributes
-  //As the state changes we re render the components
+  
+//Understanding life cycle methodes 
+//this methode is called whenever the page gets renered for the first time 
+// synonym to the oncreate life cycle methode that we used in android 
+componentDidMount(){
+
+  //javascript native api call
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response=> response.json())
+   .then(users => this.setState({monsters: users}));
+
+}
   render(){
     return (
       <div className="App">
         {
-          //RENDER AREA
-          //calls the function in the map every time for each element
-          //function monster(){
-          // return <h1> hello </h1>
-          //}
+        
         this.state.monsters.map(monster=>
-        <h1 key={monster.id}> This monster is {monster.name} </h1>)
+        <h1 key={monster.id}> This monster is {monster.name} {monster.username} </h1>)
         }
         
       </div>
