3e0b1412e2b35b73abf94d66e1aec784f43a68af
Paras Rawat <parasmyname@gmail.com>
2020-01-26 23:01:13 +0530

    Basic Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+    this.state={
+     monsters: [
+       {
+         name: 'FRANKESTIEN',
+         id: 'asc1'
+       },
+       {
+        name: 'DRACULA',
+        id: 'asc2'
+      },
+      {
+        name: 'ZOMBIE',
+        id: 'asc3'
+      }
+     ]
+    }
+  }
+  //Anything in the {} means that is a expression. Although it may look like a normal html but it is not that, it is JSX ...jsx attributes
+  //As the state changes we re render the components
+  render(){
+    return (
+      <div className="App">
+        {
+          //RENDER AREA
+          //calls the function in the map every time for each element
+          //function monster(){
+          // return <h1> hello </h1>
+          //}
+        this.state.monsters.map(monster=>
+        <h1 key={monster.id}> This monster is {monster.name} </h1>)
+        }
+        
+      </div>
+    );
+  }
 }
 
 export default App;
