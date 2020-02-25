588dbd4475afdb9159650a97911940bb1dab5a99
Paras Rawat <parasmyname@gmail.com>
2020-02-25 23:23:50 +0530

    Search Box component, Destructuring in javascript

--- src/App.js
+++ src/App.js
@@ -1,43 +1,48 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component'
+import {SearchBox} from './components/search-box/search-box.component'
 
-class App extends Component{
-  constructor(){
+class App extends Component {
+  constructor() {
     super();
     //state itself is a javascript object with many properties 
-    this.state={
-     monsters: [],
-     searchField: ''
+    this.state = {
+      monsters: [],
+      searchField: ''
     };
   }
 
 
-componentDidMount(){
-  fetch('https://jsonplaceholder.typicode.com/users')
-  .then(response=> response.json())
-   .then(users => this.setState({monsters: users}));
-   
-}
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
 
-//component recieves the state as a prop.. 
+  }
+
+  //component recieves the state as a prop.. 
 
 
-  render(){
-    const { monsters, searchField} = this.state; // javascript syntax for the destructuring of a object
+  render() {
+    const { monsters, searchField } = this.state; // javascript syntax for the destructuring of a object
     //above line of code is equivalent to const monsters= this.state.monsters && const searchfield = this.state.searchfield
-    const filteredmonsters = monsters.filter(monster => 
+    const filteredmonsters = monsters.filter(monster =>
       monster.name.toLowerCase().includes(searchField.toLowerCase())
-      )
+    )
 
+    //keynote= whenever set state is called, properties of state changes and react re renders our component, it recalls the render methode
 
     return (
       //asynchrnous is an event that takes an unknown amount of time and thus the system does not wait for it to finish and calls the next functions
       <div className="App">
-        <input type='search'  placeholder='search monsters' onChange={e => this.setState({searchField: e.target.value})
-      }/>
-        <CardList monsters={filteredmonsters}/> 
+
+        <SearchBox
+        placeholder='search monsters'
+        handleChange= {e => this.setState({ searchField: e.target.value })}
+        ></SearchBox>
+        <CardList monsters={filteredmonsters} />
       </div>
     );
   }
---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './search-box.styles.css';
+
+// a functional component is just a component that gets some props and return some jsx
+//destructuring and passing
+export const SearchBox = (props) =>{
+  // or we could simply do javascript destructuring ({placeholder, handleChange})
+    const placeholder= props.placeholder;
+    const handleChange =props.handleChange;
+
+return(
+    <input type='search' 
+    className ='search'
+    placeholder={placeholder}// keeping it dynamic 
+    onChange={handleChange} />
+)
+}
